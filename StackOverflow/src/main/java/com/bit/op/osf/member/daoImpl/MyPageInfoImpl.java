20fefe3783a35823ef3110213f5e2c600d218996
dome2073 <wlgns2073@gmail.com>
2018-10-12 18:39:27 +0900

    2018.10.12 jam9

---
+++ StackOverflow/src/main/java/com/bit/op/osf/member/daoImpl/MyPageInfoImpl.java
@@ -0,0 +1,60 @@
+package com.bit.op.osf.member.daoImpl;
+
+import java.io.File;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bit.op.osf.job.daoImpl.DaoImpl;
+import com.bit.op.osf.member.SHA256.SHA256;
+import com.bit.op.osf.member.dao.MemberInfoDao;
+import com.bit.op.osf.member.dao.MyPageInfoDao;
+import com.bit.op.osf.member.model.MemRegInfo;
+import com.bit.op.osf.tagBoard.model.QuestionBoard;
+import com.bit.op.osf.tagBoard.model.ReplyBoard;
+
+@Repository
+public class MyPageInfoImpl extends DaoImpl implements MyPageInfoDao {
+
+	
+
+	private static final String MYPAGEINFO_NAMESPACE = "com.bit.op.osf.member.mapper.MypageInfoMapper.";
+
+
+/*
+	public MemRegInfo selectMember(String id, String pw) {
+
+	
+		// 비밀번호 SHA암호화로 변경
+		System.out.println("pw       "+pw);  
+		
+
+	
+		MemRegInfo memInfo = sqlSession.selectOne(MYPAGEINFO_NAMESPACE +"selectId",id);
+		System.out.println("memInfo       "+memInfo);  
+		
+		return memInfo;
+	}
+*/
+
+
+	@Override
+	public List<ReplyBoard> selectAnswerInfo(String memId) throws Exception {
+		// TODO Auto-generated method stub   sqlSession.selectList(QUSETION_NAMESPACE + "selectQuestion", search);
+		return null;
+	}
+
+
+
+	@Override
+	public List<QuestionBoard> selectQuestionInfo(String memId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- StackOverflow/src/main/java/com/bit/op/osf/tagBoard/dao/IQuestionBoardDao.java
+++ StackOverflow/src/main/java/com/bit/op/osf/tagBoard/dao/IQuestionBoardDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import com.bit.op.osf.member.model.MemRegInfo;
 import com.bit.op.osf.tagBoard.model.QuestionBoard;
 import com.bit.op.osf.tagBoard.model.QuestionBoardList;
 import com.bit.op.osf.tagBoard.model.Search;
@@ -24,7 +25,7 @@ public interface IQuestionBoardDao {
 
 	int deleteQuestionBoard(int questionBoardNo);
 
-	QuestionBoardList selectQuestionList(Search search, HttpServletRequest request);
+	QuestionBoardList selectQuestionList(Search search,MemRegInfo memInfo);
 
 	List<QuestionBoard> selectVisitQuestion(Cookie[] cookies);
 
